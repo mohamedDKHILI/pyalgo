38833f1cb4307f8e33dd53bec9178b5af4c68641
Mohamed <mohamed.dkhili12@gmail.com>
2019-04-03 12:11:49 +0200

    mise a jour des commentaires

---
+++ hello.py
@@ -0,0 +1,14 @@
+ """
+Algorithm #0.0.1
+Declare var
+Store some arithmetics into the varDisplay result
+Operands and Operators, functions
+"""
+
+resultat = 0 # Définition de la variable
+resultat = 2 + 2
+print(resultat)
+
+"""
+Fin de l'algorithme
+"""
