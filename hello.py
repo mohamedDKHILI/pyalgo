fe54f5938514bee7a33ab3d394dff6d09954abf8
Mohamed <mohamed.dkhili12@gmail.com>
2019-04-03 13:04:35 +0200

    Merge devel

---
+++ hello.py
@@ -0,0 +1,16 @@
+ """
+Algorithm #0.0.1
+Declare var
+Store some arithmetics into the varDisplay result
+Operands and Operators, functions
+"""
+
+resultat = 0 # Définition de la variable
+operande1 = -3
+operande2 = 2
+resultat = operande1 + operande2
+print(resultat)
+
+"""
+Fin de l'algorithme
+"""
