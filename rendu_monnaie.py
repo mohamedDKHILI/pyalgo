41b55d73cac61f05b1d42cdfc333287a32a3bab4
Mohamed <mohamed.dkhili12@gmail.com>
2019-04-05 16:50:12 +0200

    derniere fonctionnalité rajout de un euro

--- rendu_monnaie.py
+++ rendu_monnaie.py
@@ -12,11 +12,13 @@ Use input () function to a user entry
 nbBillet100 = 0
 nbBillet50 = 0
 nbBillet10 = 0
-nbPiece = 0
+nbPiece2 = 0
+nbPiece1 =0
 
 
 
-entreeSomme = 470 
+
+entreeSomme = 26
 somme = int(entreeSomme)
 
 
@@ -26,16 +28,30 @@ reste100 = somme % 100
 if reste100 > 0:
     # got 50
     nbBillet50 = reste100 // 50
-    reste50 = somme % 50
+    reste50 = reste100 % 50
+
+
+
 
 
     if  reste50 > 0:
         # got 10
         nbBillet10 = reste50 // 10
-        reste10 = somme % 10
+        reste10 = reste50 % 10
 
         if reste10 > 0:
-            nbPiece = reste10 // 2
-            nbPiece = reste10 % 2
+            nbPiece2 = reste10 // 2
+            reste2 = reste10 % 2 
 
-print("la machine vous rend: ", nbBillet100, "billets de 100e", nbBillet50, "billets de 50e", nbBillet10, "Billets de 10e") 
+            if reste2 > 0:
+                nbPiece1 = reste2 // 1
+                reste1 =reste2 % 1
+
+
+        
+
+            
+         
+
+
+print("la machine vous rend: ", nbBillet100, "billets de 100e", nbBillet50, "billets de 50e", nbBillet10, "Billets de 10e", nbPiece2, "pieces de 2eu", nbPiece1, "pieces de 1eu") 
